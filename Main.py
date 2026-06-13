5571b7ba4c4d977d02c71fdacb70537bb9c9b71a
Avin <avinineramz@gmail.com>
2026-06-13 20:39:08 +0330

    Before random library

--- Main.py
+++ Main.py
@@ -1,3 +1,6 @@
+# Importing the random library to generate random HP damage
+import random
+
 # Game Menu
 print("\n( Game Start )")
 print("1. Fight \n2. Exit")
@@ -5,6 +8,8 @@ print("1. Fight \n2. Exit")
 user_input = input ("> ")
 # Starting HP for the enemy
 enemy_hp = 25
+# Starting HP for the player
+player_hp = 20
 
 # Keep the game running until the player exits
 while True:
@@ -13,8 +18,16 @@ while True:
         if attack_choice == "1":
             enemy_hp -= 5
             print(f"Goblin HP: {enemy_hp}")
+            print("Goblin attacks back!")
+            player_hp -= 5
+            print(f"Your HP: {player_hp}")
+
         else:
             print("You've lost!")
+            break
+
+        if player_hp <= 0:
+            print("Goblin Wins!")
             break
 
         if enemy_hp <= 0:
